3da0698d3590ea909ee4238f661cd8aef3e45508
mrouffet <maximerouffet@gmail.com>
2021-01-15 15:27:52 +0100

    [Update] Octree no empty cells

--- SpacialPartitioning/Assets/Scripts/Octree/OctreeNode.cs
+++ SpacialPartitioning/Assets/Scripts/Octree/OctreeNode.cs
@@ -8,35 +8,64 @@ public class OctreeNode : OctreeNodeBase
 
 	public OctreeNode(Bounds _bounds, OctreeNode _parent) : base(_bounds, _parent)
 	{
-		Vector3 size = _bounds.size / 2.0f;
-		Vector3 center = _bounds.center + size / 2.0f;
+	}
 
-		children[0] = new OctreeCell(new Bounds(center, size), this);
+	OctreeNodeBase CreateChild(int _index)
+	{
+		Vector3 size = bounds.size / 2.0f;
+		Vector3 center = bounds.center + size / 2.0f;
 
-		center.z -= size.z;
-		children[1] = new OctreeCell(new Bounds(center, size), this);
+		if(_index == 0)
+			children[0] = new OctreeCell(new Bounds(center, size), this);
 
-		center.z += size.z;
-		center.y -= size.y;
-		children[2] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 1)
+		{
+			center.z -= size.z;
+			children[1] = new OctreeCell(new Bounds(center, size), this);
+		}
 
-		center.z -= size.z;
-		children[3] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 2)
+		{
+			center.y -= size.y;
+			children[2] = new OctreeCell(new Bounds(center, size), this);
+		}
 
-		center.z += size.z;
-		center.y += size.y;
-		center.x -= size.x;
-		children[4] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 3)
+		{
+			center.y -= size.y;
+			center.z -= size.z;
+			children[3] = new OctreeCell(new Bounds(center, size), this);
+		}
 
-		center.z -= size.z;
-		children[5] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 4)
+		{
+			center.x -= size.x;
+			children[4] = new OctreeCell(new Bounds(center, size), this);
+		}
 
-		center.z += size.z;
-		center.y -= size.y;
-		children[6] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 5)
+		{
+			center.x -= size.x;
+			center.z -= size.z;
+			children[5] = new OctreeCell(new Bounds(center, size), this);
+		}
 
-		center.z -= size.z;
-		children[7] = new OctreeCell(new Bounds(center, size), this);
+		if (_index == 6)
+		{
+			center.x -= size.x;
+			center.y -= size.y;
+			children[6] = new OctreeCell(new Bounds(center, size), this);
+		}
+
+		if (_index == 7)
+		{
+			center.x -= size.x;
+			center.y -= size.y;
+			center.z -= size.z;
+			children[7] = new OctreeCell(new Bounds(center, size), this);
+		}
+
+		return children[_index];
 	}
 
 	int GetChildFromPosition(Vector3 _pos)
@@ -109,6 +138,17 @@ public class OctreeNode : OctreeNodeBase
 
 		OctreeNodeBase child = children[childIndex];
 
+		// First time creation.
+		if(child == null)
+		{
+			child = CreateChild(childIndex);
+
+			child.Insert(_obj);
+
+			return;
+		}
+
+
 		// Child is already a node.
 		{
 			OctreeNode nodeChild = child as OctreeNode;
@@ -143,6 +183,44 @@ public class OctreeNode : OctreeNodeBase
 
 	public override sealed void Remove(OctreeObj _obj)
 	{
+		int childIndex = GetChildFromPosition(_obj.transform.position);
+
+		// Child not found.
+		if (childIndex == -1)
+		{
+			Debug.LogError("Child not found, object may be out of bound!");
+			return;
+		}
+
+		OctreeNodeBase child = children[childIndex];
+
+		if(child == null)
+		{
+			Debug.LogWarning("Remove object from an empty cell.");
+			return;
+		}
+
+
+		// Child is a node.
+		{
+			OctreeNode nodeChild = child as OctreeNode;
+
+			if (nodeChild != null)
+			{
+				nodeChild.Remove(_obj);
+				return;
+			}
+		}
+
+		// Child is a cell.
+		{
+			OctreeCell cellChild = child as OctreeCell;
+
+			cellChild.objects.Remove(_obj);
+
+			if (cellChild.objects.Count == 0) // Empty cell?
+				children[childIndex] = null;
+		}
 	}
 
 	public sealed override void OnDrawGizmos(OctreeDrawDebugInfos debugInfos)
@@ -155,7 +233,10 @@ public class OctreeNode : OctreeNodeBase
 		debugInfos.AddDepth();
 
 		for (int i = 0; i < 8; ++i)
-			children[i].OnDrawGizmos(debugInfos);
+		{
+			if(children[i] != null)
+				children[i].OnDrawGizmos(debugInfos);
+		}
 
 		debugInfos.RemoveDepth();
 
